eef5b097af1e19db1daf98c31bd1c46adac7f803
mushah9l@gmail.com <PrTerra905643Topi4>
2021-03-15 17:24:55 +0700

    Интеграция с Jenkins headless true

--- src/main/java/WileyConfigUI.java
+++ src/main/java/WileyConfigUI.java
@@ -1,17 +1,14 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import io.qameta.allure.selenide.LogType;
 import org.testng.annotations.BeforeClass;
-
-import java.util.logging.Level;
 
 
 public class WileyConfigUI {
 
     @BeforeClass
     public void setUp() {
+        Configuration.headless = true;
         Configuration.startMaximized = true;
         Configuration.baseUrl = "https://www.wiley.com/en-us";
         Configuration.savePageSource = false;
