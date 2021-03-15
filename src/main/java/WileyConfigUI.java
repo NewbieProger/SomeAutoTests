85021ad4b3de7e0587f95378d38920bf42114f72
mushah9l@gmail.com <PrTerra905643Topi4>
2021-03-15 18:35:06 +0700

    Добавил листенер на подсветку элементов

--- src/main/java/WileyConfigUI.java
+++ src/main/java/WileyConfigUI.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
 
@@ -18,6 +20,7 @@ public class WileyConfigUI {
         SelenideLogger.addListener("AllureReport", new AllureSelenide().includeSelenideSteps(true)
                 .savePageSource(false)
                 .screenshots(true));
+        WebDriverRunner.addListener(new HighLighterListener());
 
     }
 }
